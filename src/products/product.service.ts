3c5360535db20228d82c4a7f9b40ce0506e55372
Hasibur Rahman <hasibur.rahman@brainstation-23.com>
2024-10-14 16:02:07 +0600

    add product service code

---
+++ src/products/product.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma, PrismaClient, Product } from '@prisma/client';
+
+@Injectable()
+export class ProductService {
+  constructor(private prisma: PrismaService) {}
+
+  async createProduct(data: Prisma.ProductCreateInput): Promise<Product> {
+    return this.prisma.product.create({
+      data: {
+        ...data,
+      },
+    });
+  }
+  async updateProduct(
+    id: number,
+    data: Prisma.ProductUpdateInput,
+  ): Promise<Product> {
+    return this.prisma.product.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async getAllProducts(
+    page: number,
+    limit: number,
+    filter?: { category?: string },
+  ): Promise<Product[]> {
+    const skip = (page - 1) * limit;
+
+    return this.prisma.product.findMany({
+      skip: skip,
+      take: limit,
+      where: {
+        ...(filter?.category && { category: { equals: filter.category } }),
+      },
+    });
+  }
+
+  async deleteProduct(id: number) {
+    return this.prisma.product.delete({ where: { id } });
+  }
+}
