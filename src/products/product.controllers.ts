59b52a629cac528eaa09e4325a5f5f541da949bd
hasibur Rahman <hasibhr31@gmail.com>
2024-10-22 16:15:54 +0600

    product api documentation

--- src/products/product.controllers.ts
+++ src/products/product.controllers.ts
@@ -1,16 +1,42 @@
 import { Body, Controller, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto ,UpdateProductDto} from './product.dto';
+import { ApiTags,ApiOperation,ApiResponse, ApiQuery } from '@nestjs/swagger';
 
+@ApiTags('product')
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new product' })
+  @ApiResponse({
+    status: 201,
+    description: 'Product has been successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'Invalid input.' })
   createProduct(@Body() createProductDto: CreateProductDto) {
     return this.productService.createProduct(createProductDto);
   }
   @Get('')
+  @ApiOperation({ summary: 'Retrieve all products' })
+  @ApiResponse({
+    status: 200,
+    description: 'All products have been successfully retrieved.',
+    type: [CreateProductDto],
+  })
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    type: Number,
+    description: 'Page number for pagination',
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    type: Number,
+    description: 'Number of products per page',
+  })
   getAllProducts(
     @Query('page') page: number = 1,
     @Query('limit') limit: number = 10,
@@ -19,7 +45,17 @@ export class ProductController {
   }
 
   @Patch(':id')
-  updateProduct(@Param('id') id:string ,@Body() updateProductDto: UpdateProductDto){
-    return this.productService.updateProduct(+id,updateProductDto);
+  @ApiOperation({ summary: 'Update a product by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'The product has been successfully updated.',
+  })
+  @ApiResponse({ status: 404, description: 'Product not found.' })
+  @ApiResponse({ status: 400, description: 'Invalid input.' })
+  updateProduct(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productService.updateProduct(+id, updateProductDto);
   }
 }
--- src/products/product.dto.ts
+++ src/products/product.dto.ts
@@ -1,43 +1,57 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmpty, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({ description: 'Name of the product' })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({ description: 'Description of the product' })
   @IsNotEmpty()
   @IsString()
   description: string;
 
+  @ApiProperty({ description: 'Price of the product' })
   @IsNotEmpty()
   @IsNumber()
   price: number;
 
+  @ApiProperty({ description: 'Quantity of the product' })
   @IsNotEmpty()
   @IsNumber()
   stockQuantity: number;
 
+  @ApiProperty({ description: 'Category of the product' })
   @IsNotEmpty()
   @IsString()
   category: string;
 }
 export class UpdateProductDto {
+  @ApiProperty({ description: 'Name of the product', required: false })
   @IsString()
   @IsOptional()
   name?: string;
 
+  @ApiProperty({ description: 'Description of the product', required: false })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({ description: 'Price of the product', required: false })
   @IsNumber()
   @IsOptional()
   price?: number;
 
+  @ApiProperty({ description: 'StockQuantity of the product', required: false })
   @IsNumber()
   @IsOptional()
   stockQuantity?: number;
-  
+
+  @ApiProperty({
+    description: 'Optional category of the product',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   category?: string;
